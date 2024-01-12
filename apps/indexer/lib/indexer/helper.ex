bd2ca0b4ef4f5e6fdfed993d391f672f878304d5
POA <33550681+poa@users.noreply.github.com>
2024-01-12 10:49:47 +0300

    Remove redundant function

--- apps/indexer/lib/indexer/helper.ex
+++ apps/indexer/lib/indexer/helper.ex
@@ -105,15 +105,6 @@ defmodule Indexer.Helper do
     end
   end
 
-  @spec log_topic_to_string(any()) :: binary() | nil
-  def log_topic_to_string(topic) do
-    if is_binary(topic) or is_nil(topic) do
-      topic
-    else
-      Hash.to_string(topic)
-    end
-  end
-
   @doc """
   Converts a log topic from Hash.Full representation to string one.
   """
